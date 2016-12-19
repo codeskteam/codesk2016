e729bd31b84a921735c568f1e11db4c8d9f3e22c
JF <jan@faessler.be>
2016-12-19 18:30:55 +0100

    removed readme.md and added website stuff

---
+++ brunch/brunch-config.js
@@ -0,0 +1,22 @@
+// See http://brunch.io for documentation.
+exports.files = {
+  javascripts: {joinTo: 'app.js'},
+  stylesheets: {joinTo: 'app.css'},
+  templates: {joinTo: 'app.js'}
+};
+
+// exports.config {
+// 	files = {
+// 		javascripts: {joinTo: 'app.js'},
+// 		stylesheets: {joinTo: 'app.css'},
+// 		templates: {joinTo: 'app.js'}
+// 	},
+// 	plugins: {
+// 		sass: {
+// 			options: {
+// 				includePaths: ["app/scss"] // tell sass-brunch where to look for files to @import
+// 				// precision: 8 // minimum precision required by bootstrap-sass
+// 			}
+// 		}
+// 	}
+// }
