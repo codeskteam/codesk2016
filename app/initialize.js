9d1dc9a7ac2d1f126c03bce183d9a55851dd8dec
JF <jan@faessler.be>
2016-12-20 21:20:13 +0100

    added jquery to package json

--- app/initialize.js
+++ app/initialize.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	var dstamp = new Date();
-	var hourstamp = dstamp.getHours();
-
-	if (hourstamp > 10 && hourstamp < 15) {
-		// console.log("time between 10 and 15 o clock = DAY");
-	} else if (hourstamp > 15 || hourstamp < 10) {
-		// console.log("time between 15 and 24 or 00 and 10 o clock = NIGHT");
-	}
+	// var dstamp = new Date();
+	// var hourstamp = dstamp.getHours();
+	//
+	// if (hourstamp > 10 && hourstamp < 15) {
+	// 	// console.log("time between 10 and 15 o clock = DAY");
+	// } else if (hourstamp > 15 || hourstamp < 10) {
+	// 	// console.log("time between 15 and 24 or 00 and 10 o clock = NIGHT");
+	// }
 
 });
